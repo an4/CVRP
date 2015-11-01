5d8cba0c1d26bc90fabb1c6fd66224776f0ad866
an4 <ad12461@my.bristol.ac.uk>
2015-11-01 17:15:37 +0000

    .

--- CVRPData.java
+++ CVRPData.java
@@ -9,7 +9,7 @@ import java.awt.Point;
  * parameter checking to make sure that nodes passed to the get
  * methods are within the valid range.
  *
- * It should be initialised by calling readFile(). 
+ * It should be initialised by calling readFile().
  *
  * @author Tim Kovacs
  * @version 20/10/2013
@@ -34,26 +34,26 @@ public class CVRPData {
     /** Return the demand for a given node. */
     public static int getDemand(int node) {
 	if (!nodeIsValid(node)) {
-	    System.err.println("Error: demand for node " + node + 
+	    System.err.println("Error: demand for node " + node +
 			       " was requested from getDemand() but only nodes 1.." + NUM_NODES + " exist");
 	    System.exit(-1);
-	}	    
+	}
 	return demand[node];
     }
-        
+
     /** Return the Euclidean distance between the two given nodes */
     public static double getDistance(int node1, int node2) {
 	if (!nodeIsValid(node1)) {
-	    System.err.println("Error: distance for node " + node1 + 
+	    System.err.println("Error: distance for node " + node1 +
 			       " was requested from getDistance() but only nodes 1.." + NUM_NODES + " exist");
 	    System.exit(-1);
-	}	    
-	
+	}
+
 	if (!nodeIsValid(node2)) {
-	    System.err.println("Error: distance for node " + node2 + 
+	    System.err.println("Error: distance for node " + node2 +
 			       " was requested from getDistance() but only nodes 1.." + NUM_NODES + " exist");
 	    System.exit(-1);
-	}	    
+	}
 
 	int x1 = coords[node1][X_COORDINATE];
 	int y1 = coords[node1][Y_COORDINATE];
@@ -68,7 +68,7 @@ public class CVRPData {
     // 44-13 = 31 and 31^2 = 961
     // 121 + 961 = 1082
     // The square root of 1082 = 32.893768...
-    }    
+    }
 
     //* Return the absolute co-ordinates of the given node */
     public static Point getLocation(int node) {
@@ -87,12 +87,12 @@ public class CVRPData {
 	else
 	    return true;
     }
-    
+
     private static final int X_COORDINATE = 0; // x-axis coordinate is dimension 0 in coords[][]
     private static final int Y_COORDINATE = 1; // y-axis coordinate is dimension 1 in coords[][]
     private static final int NUM_COORDINATE_DIMENSIONS = 2;
 
-    // 2-dimensional array with the coordinates of each node in the .vrp file. 
+    // 2-dimensional array with the coordinates of each node in the .vrp file.
     // coords[0] is a dummy entry which is not used. Node 1 is at coords[1], node 2 is at coords[2] and so on.
     // A more Object-Oriented solution would have been to create node objects. That would probably have been better.
     private static int [][] coords;
@@ -103,38 +103,38 @@ public class CVRPData {
     private static final String CAPACITY_PREFIX = "CAPACITY : ";
 
     /** Read the .vrp data file into arrays. This method does minimal format checking on the file */
-    public static void readFile(String file) {	
+    public static void readFile(String file) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
 	    String input;
 
 	    // read first line, which tells us the number of nodes in data file
-	    input = reader.readLine(); 
+	    input = reader.readLine();
 	    if (!input.startsWith(DIMENSION_PREFIX))
 		quit("Error: first line must begin with \"" + DIMENSION_PREFIX + "\"");
 	    // extract the text which follows the prefix
-	    input = input.substring(DIMENSION_PREFIX.length()); 
+	    input = input.substring(DIMENSION_PREFIX.length());
 	    NUM_NODES = Integer.valueOf(input);
 
 	    // create arrays to hold the data we will read from the file
 	    // +1 because we don't use index 0
 	    coords = new int[NUM_NODES+1][NUM_COORDINATE_DIMENSIONS];
 	    demand = new int[NUM_NODES+1];
-	    
+
 	    // read second line, which tells us the capacity each truck has
-	    input = reader.readLine(); 
+	    input = reader.readLine();
 	    if (!input.startsWith(CAPACITY_PREFIX))
 		quit("Error: second line must begin with \"" + CAPACITY_PREFIX + "\"");
-	    input = input.substring(CAPACITY_PREFIX.length()); 
+	    input = input.substring(CAPACITY_PREFIX.length());
 	    VEHICLE_CAPACITY = Integer.valueOf(input);
 
 	    // read line saying "NODE_COORD_SECTION
-	    input = reader.readLine(); 
+	    input = reader.readLine();
 
 	    // read each node coordinate and add it to coords[][]
 	    String[] words;
 	    for (int node=1; node<= NUM_NODES; node++) {
-		input = reader.readLine(); 	 
+		input = reader.readLine();
 		words = input.split(" "); // format: node number, x-coordinate, y-coordinate
 		coords[node][X_COORDINATE] = Integer.valueOf(words[1]);
 		coords[node][Y_COORDINATE] = Integer.valueOf(words[2]);
@@ -148,19 +148,19 @@ public class CVRPData {
 
 	    // read the demand for each node
 	    for (int node=1; node<= NUM_NODES; node++) {
-		input = reader.readLine(); 	 
+		input = reader.readLine();
 		words = input.split(" "); // format: node number, demand
 		demand[node] = Integer.valueOf(words[1]);
 		//System.out.println(node + " " + demand[node]);
 	    }
-	    reader.close();                         	
+	    reader.close();
 	}
         catch(FileNotFoundException e) {
             quit("Error: unable to open file " + file);
         }
         catch(IOException e) {
             quit("Error: an I/O problem occurred attempting to read " + file);
-        }   
+        }
 
     }
 
