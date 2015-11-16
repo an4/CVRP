4ace2d7fdab4fd2805a1f7a9a7efbd0ffc44c02c
an4 <ad12461@my.bristol.ac.uk>
2015-11-16 20:32:24 +0000

    .

--- Chromosome.java
+++ Chromosome.java
@@ -1,16 +1,23 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 class Chromosome {
-  final static int DIMENSION = 250;
+  final static int DIMENSION = Data.DIMENSION;
 
   private Integer[] genes = new Integer[DIMENSION];
   private Double distance = 0.0;
   private Double fitness = 0.0;
 
-  public Chromosome(Integer[] genes, Double distance) {
+  public Chromosome() {
+    this.genes = getPermutation();
+    this.distance = computeDistance(this.genes);
+    this.fitness = 1.0/this.distance;
+  }
+
+  public Chromosome(Integer[] genes) {
     this.genes = Arrays.copyOf(genes, genes.length);
-    this.distance = distance;
-    this.fitness = 1.0/distance;
+    this.distance = computeDistance(genes);
+    this.fitness = 1.0/this.distance;
   }
 
   public Double getFitness() {
@@ -23,5 +30,25 @@ class Chromosome {
 
   public Double getDistance() {
     return this.distance;
+  }
+
+  static double computeDistance(Integer[] genes) {
+    double distance = 0.0;
+    for(int i=1; i<genes.length; i++) {
+      distance += Data.EDM[genes[i]][genes[i-1]];
+    }
+    return distance + Data.EDM[genes[genes.length-1]][0];
+  }
+
+  static Integer[] getPermutation() {
+    Integer[] array = new Integer[DIMENSION];
+    for(int i=0; i<DIMENSION; i++) {
+      array[i] = i;
+    }
+    Collections.shuffle(Arrays.asList(array));
+    if(array[0] != 0) {
+      Solution.swapPosition(0, array[0], array);
+    }
+    return array;
   }
 }
--- Population.java
+++ Population.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
 public class Population {
+  private Chromosome[] population;
 
+  private double avgDistance = 0.0;
+
+  private double minDistance = 0.0;
+
+  private double maxDistance = Double.MAX_VALUE;
+
+  public Population() {
+    
+  }
 }
