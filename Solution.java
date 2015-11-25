0dfb5e96ce8d7b43240be42a8dfc17027a9a4011
an4 <ad12461@my.bristol.ac.uk>
2015-11-25 23:51:11 +0000

    .

--- Solution.java
+++ Solution.java
@@ -165,35 +165,106 @@ public class Solution{
     return new Population(list.toArray(new Chromosome[list.size()]), size);
   }
 
+  /* Simple solution for spliting the best TSP route into smaller routes fro CVRP */
   public static void getSimpleSolution(Population population) {
     Integer[] best_route = population.getChromosomes()[0].getGenes();
-    int vehicles = 1;
     int capacity = CAPACITY;
     double cost = 0;
     StringBuilder br = new StringBuilder();
+    ArrayList<ArrayList<Integer>> routes = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> route = new ArrayList<Integer>();
     br.append("1->");
+    route.add(0);
     int i=0;
     for(i=1; i<DIMENSION; i++) {
       if(capacity - Data.demand[best_route[i]] > 0) {
         capacity -= Data.demand[best_route[i]];
         cost += Data.EDM[best_route[i]][best_route[i-1]];
         br.append((best_route[i]+1) + "->");
+        route.add(best_route[i]);
       } else {
         capacity = CAPACITY - Data.demand[best_route[i]];
-        vehicles++;
         cost += Data.EDM[best_route[i-1]][0];
         cost += Data.EDM[0][best_route[i]];
         br.append("1\n");
+        route.add(0);
+        routes.add(route);
+        route = new ArrayList<Integer>();
+        route.add(0);
+        route.add(best_route[i]);
         br.append("1->" + (best_route[i]+1) + "->");
       }
     }
     cost += Data.EDM[best_route[i-1]][0];
     br.append("1");
+    route.add(0);
+    routes.add(route);
 
     printSolution(br.toString(), cost);
-
-    System.out.println("C: " + cost);
+    System.out.println("SSC: " + cost);
   }
+
+  public static double getRouteCost(ArrayList<Integer> route) {
+    double cost = 0.0;
+    for(int i=1; i<route.size(); i++) {
+      cost += Data.EDM[route.get(i-1)][route.get(i)];
+    }
+    return cost;
+  }
+
+  public static void randomIntervalSolution(Population population) {
+    Random random = new Random();
+    Integer[] best_route = population.getChromosomes()[0].getGenes();
+    StringBuilder br = new StringBuilder();
+
+    ArrayList<ArrayList<Integer>> best_routes = null;
+    double best_cost = Double.MAX_VALUE;
+
+    for(int round=0; round<50; round++) {
+      ArrayList<ArrayList<Integer>> routes = new ArrayList<ArrayList<Integer>>();
+      int capacity = CAPACITY;
+      double cost = 0.0;
+      int i=0;
+      int last = 0;
+      for(i=1; i<DIMENSION; i++) {
+        if(capacity - Data.demand[best_route[i]] > 0) {
+          capacity -= Data.demand[best_route[i]];
+        } else {
+          int rand = random.nextInt(i - last - 1) + last + 1;
+
+          ArrayList<Integer> route = new ArrayList<Integer>();
+          route.add(0);
+          for(int k=last; k<rand; k++) {
+            route.add(best_route[k]);
+          }
+          route.add(0);
+          cost += getRouteCost(route);
+          routes.add(route);
+
+          last = rand;
+          capacity = CAPACITY - Data.demand[best_route[i]];
+        }
+      }
+      ArrayList<Integer> route = new ArrayList<Integer>();
+      route.add(0);
+      for(int k=last; k<DIMENSION; k++) {
+        route.add(best_route[k]);
+      }
+      route.add(0);
+      cost += getRouteCost(route);
+      routes.add(route);
+
+      if(cost < best_cost) {
+        best_cost = cost;
+        best_routes = new ArrayList<ArrayList<Integer>>(routes);
+      }
+    }
+
+    // printSolution(br.toString(), cost);
+    System.out.println("Rand: " + best_cost);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   public static void printSolution(String routes, Double cost) {
     try {
@@ -203,7 +274,7 @@ public class Solution{
       file.println("algorithm Genetic Algorithm with crossover and mutation");
 
       file.println("cost " + cost);
-      file.println(routes);
+      file.print(routes);
       file.close();
     }
     catch (Exception ex)
@@ -289,47 +360,81 @@ public class Solution{
     return next;
   }
 
-  public static double getRandomCost(ArrayList<Integer> route) {
-    double distance = 0.0;
+/******************************************************************************/
+  public static double getSmallRouteCost(ArrayList<Integer> route) {
+    double cost = 0.0;
     for(int i=1; i<route.size(); i++) {
-      distance += Data.EDM[route.get(i-1)][route.get(i)];
+      cost += Data.EDM[route.get(i-1)][route.get(i)];
     }
-    return distance;
+    return cost;
   }
 
-  public static boolean isRouteValid(ArrayList<Integer> route) {
-    int cost = 0;
-    for(Integer x: route) {
-      if(x == 0) {
-        if(cost + Data.demand[x] > CAPACITY) {
-          return false;
-        }
-        cost = 0;
-      } else {
-        cost += Data.demand[x];
-      }
-      if(cost >  CAPACITY) {
-        return false;
-      }
+  public static void smallerRoutes(ArrayList<ArrayList<Integer>> routes) {
+    int total_routes_number = routes.size();
+    double cost[] = new double[total_routes_number];
+    double total_cost = 0.0;
+    for(int i=0; i<total_routes_number; i++) {
+      cost[i] = getSmallRouteCost(routes.get(i));
+      total_cost += cost[i];
     }
-    return true;
-  }
 
-  public static void randomTrucks(Integer[] genes) {
-    ArrayList<Integer> routes = new ArrayList<Integer>(Arrays.asList(genes));
+    System.out.println("C: " + total_cost);
+
     Random random = new Random();
-    while(!isRouteValid(routes)) {
-      int stop = random.nextInt(routes.size()-1) + 1;
-      routes.add(stop, 0);
+
+    for(int i=0; i<total_routes_number-1; i++) {
+      for(int j=i+1; j<total_routes_number; j++) {
+        for(int k=0; k<500; k++) {
+          int a = random.nextInt(routes.get(i).size()-2) + 1;
+          int b = random.nextInt(routes.get(j).size()-2) + 1;
+          int city_a = routes.get(i).get(a);
+          int city_b = routes.get(j).get(b);
+
+          ArrayList<Integer> temp_a = new ArrayList<Integer>(routes.get(i));
+          ArrayList<Integer> temp_b = new ArrayList<Integer>(routes.get(j));
+
+          temp_a.set(a, city_b);
+          temp_b.set(b, city_a);
+
+          double new_cost_a = getSmallRouteCost(temp_a);
+          double new_cost_b = getSmallRouteCost(temp_b);
+
+          if(new_cost_a + new_cost_b < cost[i] + cost[j] &&
+          new_cost_a <= CAPACITY && new_cost_b <= CAPACITY) {
+            routes.set(i, temp_a);
+            routes.set(j, temp_b);
+            cost[i] = new_cost_a;
+            cost[j] = new_cost_b;
+          }
+        }
+      }
     }
-    System.out.println("R: " + getRandomCost(routes));
+
+    total_cost = 0.0;
+    for(int i=0; i<total_routes_number; i++) {
+      total_cost += cost[i];
+    }
+    System.out.println("C: " + total_cost);
+
+    StringBuilder br = new StringBuilder();
+    for(int i=0; i<routes.size(); i++) {
+      for(int j=0; j<routes.get(i).size(); j++) {
+        if(j == routes.get(i).size() -1) {
+          br.append((routes.get(i).get(j) + 1) + "\n");
+        } else {
+          br.append((routes.get(i).get(j) + 1) + "->");
+        }
+      }
+    }
+    printSolution(br.toString(), total_cost);
   }
+
+/******************************************************************************/
 
   public static void main(String[] args) {
     int size = 2000;
     int generations = 4000;
-    int GA_rounds = 10;
-    int hc = 100;
+    int GA_rounds = 5;
 
     Population best_population = null;
     double cost = Double.MAX_VALUE;
@@ -347,8 +452,11 @@ public class Solution{
       }
     }
     System.out.println(best_population.getMinDistance());
+
+    // smallerRoutes(best_population.getChromosomes()[0].getGenes());
+
     getSimpleSolution(best_population);
 
-    // randomTrucks(genes);
+    randomIntervalSolution(best_population);
   }
 }
